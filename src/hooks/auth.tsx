808ae50cad62dca082924a833919d426dcb74b98
Renan Bertoldo <renan.rbertoldo@gmail.com>
2020-06-25 02:22:04 -0300

    feat: added bearer token

--- src/hooks/auth.tsx
+++ src/hooks/auth.tsx
@@ -8,9 +8,15 @@ import React, {
 import AsyncStorage from '@react-native-community/async-storage';
 import api from '../services/api';
 
+interface User {
+  id: string;
+  name: string;
+  email: string;
+  avatar_url: string;
+}
 interface AuthState {
   token: string;
-  user: object;
+  user: User;
 }
 interface SignInCreadentials {
   email: string;
@@ -18,7 +24,7 @@ interface SignInCreadentials {
 }
 
 interface AuthContextData {
-  user: object;
+  user: User;
   loading: boolean;
   signIn(credentials: SignInCreadentials): Promise<void>;
   signOut(): void;
@@ -40,6 +46,8 @@ export const AuthProvider: React.FC = ({ children }) => {
       ]);
 
       if (token[1] && user[1]) {
+        api.defaults.headers.authorization = `Bearer ${token[1]}`;
+
         setData({ token: token[1], user: JSON.parse(user[1]) });
       }
 
@@ -58,6 +66,9 @@ export const AuthProvider: React.FC = ({ children }) => {
       ['@GoBarber:token', token],
       ['@GoBarber:user', JSON.stringify(user)],
     ]);
+
+    api.defaults.headers.authorization = `Bearer ${token}`;
+
     setData({ token, user });
   }, []);
 
